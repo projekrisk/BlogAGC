b40ac5fdecbefb008cbdc7efd9bde09c8a7b161f
Projekrisk <projekrisk@gmail.com>
2026-09-08 23:08:14 +0700

    fix login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,10 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') !== 'local') {
-            URL::forceScheme('https');
-        }
-        
+        URL::forceScheme('https');
+        request()->server->set('HTTPS', 'on');
         App::setLocale('id');
         
         Carbon::setLocale('id');
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Rute untuk proses Login dengan Google Blogger
 Route::get('/auth/google', [GoogleAuthController::class, 'redirect']);
 Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback']);
+
+Route::get('/reset-hosting', function () {
+    \Illuminate\Support\Facades\Artisan::call('optimize:clear');
+    \Illuminate\Support\Facades\Artisan::call('filament:optimize-clear');
+    return 'Berhasil! Cache Hosting telah disapu bersih. Silakan akses kembali halaman login admin.';
+});
